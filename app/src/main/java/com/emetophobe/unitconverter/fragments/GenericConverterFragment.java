a7c9cd062296e24d552723f29841dffa41163356
Mike C <Mike933@gmail.com>
2015-03-20 12:14:44 -0400

    Removed unused imports.

--- app/src/main/java/com/emetophobe/unitconverter/fragments/GenericConverterFragment.java
+++ app/src/main/java/com/emetophobe/unitconverter/fragments/GenericConverterFragment.java
@@ -22,7 +22,6 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.ArrayRes;
 import android.support.annotation.NonNull;
-import android.support.v7.appcompat.BuildConfig;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
